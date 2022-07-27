2e7d9b373a349ccbec4a7268b46f50cd31ebf5c5
Radon <526745658@qq.com>
2022-07-27 22:08:30 +0800

    C语言学习第三课时

---
+++ C/3/homework1.c
@@ -0,0 +1,8 @@
+//写一个printf函数，要求在屏幕上打印内容：FishC.com no "fish"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("FishC.com no \"fish\"\n");
+	return 0;
+}
---
+++ C/3/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+	p\
+r\
+i\
+n\
+t\
+f\
+(\
+"\
+\na\n\
+	a\n\
+	a\n\
+	b\n\
+	c\n\
+	a\n\
+	a\n\
+	a\n\
+	a\n\
+	a\n");
+	return 0;
+}
